81ac3749cfd8b0ed89c16e682f7eedb5aabd3d14
michellewong27 <michellekwong2@gmail.com>
2022-11-28 12:05:19 -0500

    Final version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import SBProvider from "@sendbird/uikit-react/SendbirdProvider";
+import "./styles.css";
+import "@sendbird/uikit-react/dist/index.css";
+import MobileView from "./MobileView";
+import DesktopView from "./DesktopView";
 
 function App() {
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const USER_ID = process.env.REACT_APP_USER_ID;
+  const NICKNAME = process.env.REACT_APP_NICKNAME;
+  const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+  const myColorSet = {
+    "--sendbird-light-primary-100": "rgb(195, 241, 169)",
+    "--sendbird-light-primary-300": "rgb(165, 211, 149)",
+    "--sendbird-light-primary-400": "rgb(136, 188, 132)",
+  };
+
+  const [mobileViewOpen, setMobileViewOpen] = React.useState(false);
+  window.addEventListener("resize", function () {
+    if (window.innerWidth < 900) {
+      setMobileViewOpen(true);
+    } else {
+      setMobileViewOpen(false);
+    }
+  });
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+      setMobileViewOpen(true);
+    } else {
+      setMobileViewOpen(false);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SBProvider
+      appId={APP_ID}
+      userId={USER_ID}
+      nickname={NICKNAME}
+      accessToken={ACCESS_TOKEN}
+      colorSet={myColorSet}
+    >
+      {mobileViewOpen ? <MobileView /> : <DesktopView />}
+    </SBProvider>
   );
 }
 
